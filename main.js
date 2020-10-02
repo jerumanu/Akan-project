0592df06e7a3aa7180f0476225ce28b5ed488686
jerumanu <emmanueljeru200@gmail.com>
2020-10-02 15:11:49 +0300

    add fuction

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 var akanMaleNames = [ "Kwasi", "Kwadwo","Kwabena","Kwaku ", "Yaw","Kofi" ,"Kwame"];
 var akanFemaleNames= ["Akosua","Adwoa"," Abenaa","Akua","  Yaa","Afua"];
 
-function akan() {
-    var yy =parseInt(document.getElementById(century).value);
-    var mm =parseInt(document.getElementById(year).value);
-    var yy =parseInt(document.getElementById(month).value);
-    var yy =parseInt(document.getElementById(day).value);
 
-    
-    
-}
+    var cc =parseInt(document.getElementById(century).value);
+    var yy =parseInt(document.getElementById(year).value);
+    var mm =parseInt(document.getElementById(month).value);
+    var dd =parseInt(document.getElementById(day).value);
+     (d) = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) 
+
+
+  function akan() {
+      if (gender === "Male") {
+          prompt("Your Ghanian akan Name is " + akanMaleNames[d]);
+          
+      } else {
+        prompt("Your Ghanian akan Name is " + akanFemaleNames[d])
+          
+      }
+      
+  }
