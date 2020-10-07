45ae6537b86083ce95cd9ca543eedd8eae2aece0
jerumanu <emmanueljeru200@gmail.com>
2020-10-07 11:39:24 +0300

    modified formuler

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku ", "Yaw", "Kofi", "Kwame"];
 var akanFemaleNames = ["Akosua", "Adwoa", " Abenaa", "Akua", "  Yaa", "Afua"];
 
+
 function akan() {
 
     var cc = parseInt(document.getElementById("century").value);
@@ -8,7 +9,7 @@ function akan() {
     var mm = parseInt(document.getElementById("month").value);
     var dd = parseInt(document.getElementById("day").value);
     var gender = document.getElementById("gender").value;
-    var d = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
+    var d = parseInt((((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7)-1
 
 
     if (mm <= 0 || mm > 12) {
